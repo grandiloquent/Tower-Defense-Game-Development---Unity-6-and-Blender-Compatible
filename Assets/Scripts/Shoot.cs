a3ae3ac775418fa03a9823a81c7be1f026ab966e
grandiloquent <1785907865@qq.com>
2025-04-19 00:33:36 +0800

    Section 5 Completed

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -5,6 +5,8 @@ public class Shoot : MonoBehaviour
     GameObject currentTarget;
     public GameObject core;
     public GameObject gun;
+    Quaternion coreStartRot;
+    Quaternion gunStartRot;
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("goob"))
@@ -21,15 +23,47 @@ public class Shoot : MonoBehaviour
     }
     void Start()
     {
+        coreStartRot = core.transform.rotation;
+        gunStartRot = gun.transform.rotation;
 
+    }
+    bool IsNullOrDestroyed(System.Object obj)
+    {
+
+        if (object.ReferenceEquals(obj, null)) return true;
+
+        if (obj is UnityEngine.Object) return (obj as UnityEngine.Object) == null;
+
+        return false;
     }
     void Update()
     {
-        if (currentTarget != null)
+        if (currentTarget)
         {
-            Vector3 aimAt = new Vector3(currentTarget.transform.position.x, core.transform.position.y, currentTarget.transform.position.z);
-            core.transform.LookAt(aimAt);
+            Vector3 aimAt = new Vector3(
+                currentTarget.transform.position.x,
+            core.transform.position.y,
+            currentTarget.transform.position.z);
+
+            float distToTarget = Vector3.Distance(aimAt, gun.transform.position);
+            Vector3 relativeTarget = gun.transform.position + gun.transform.forward * distToTarget;
+            relativeTarget = new Vector3(relativeTarget.x, core.transform.position.y, relativeTarget.z);
+            gun.transform.rotation = Quaternion.Slerp(gun.transform.rotation,
+             Quaternion.LookRotation(relativeTarget - gun.transform.position), Time.deltaTime);
+            core.transform.rotation = Quaternion.Slerp(core.transform.rotation,
+            Quaternion.LookRotation(aimAt - core.transform.position), Time.deltaTime
+             );
+
         }
+        else
+        {
+            gun.transform.rotation = Quaternion.Slerp(gun.transform.rotation,
+                        gunStartRot, Time.deltaTime);
+            core.transform.rotation = Quaternion.Slerp(core.transform.rotation,
+            coreStartRot, Time.deltaTime
+             );
+        }
+
     }
     void FixedUpdate()
     {
